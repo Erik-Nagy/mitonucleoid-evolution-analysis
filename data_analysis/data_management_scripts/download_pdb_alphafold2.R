b1e7d6716c718fad7ef37aafe2653a2c5af15530
Erik Nagy <nagy256@uniba.sk>
2026-05-27 14:26:32 +0200

    create dir if it doesnt exist

--- data_analysis/data_management_scripts/download_pdb_alphafold2.R
+++ data_analysis/data_management_scripts/download_pdb_alphafold2.R
@@ -4,6 +4,8 @@ p_sites_table <- read_tsv("./data/mt_nucleoid_PTMs_list_P-sites_20260414.tsv")
 
 uniport_ids <- p_sites_table$`Uniprot ID`
 
+if(!dir.exists("./data/proteins_pdb")) dir.create("./data/proteins_pdb")
+
 for (id in uniport_ids) {
   url <- paste0("https://alphafold.ebi.ac.uk/files/AF-", id, "-F1-model_v6.pdb")
   destfile <- paste0("./data/proteins_pdb/", id, ".pdb")
